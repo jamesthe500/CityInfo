c5ad174a30aa6b93eecbf045b45ad70bdc9d71d1
James Daniels <Daniels.James@mayo.edu>
2018-08-15 14:31:09 -0500

    Overwrites default JSON retrun format. m2c12

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -16,6 +17,16 @@ namespace CityInfo.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+                // default will return JSON with first letter lowercase.
+                // This makes it so it comes back as defined in the datastore.
+                //.AddJsonOptions(o => {
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver 
+                //            as DefaultContractResolver;
+                //        castedResolver.NamingStrategy = null;
+                //    }
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
